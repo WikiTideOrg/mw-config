858c1cfb718bebc751e8276b4dc19b6fb203d926
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-12-01 14:21:24 -0700

    Remove SSL from DB

--- Database.php
+++ Database.php
@@ -16,22 +16,11 @@ $wgLBFactoryConf = [
 		'user' => $wgDBuser,
 		'password' => $wgDBpassword,
 		'type' => 'mysql',
-		'ssl' => true,
 		'flags' => DBO_DEFAULT,
 		'variables' => [
 			// https://mariadb.com/docs/reference/mdb/system-variables/innodb_lock_wait_timeout
 			'innodb_lock_wait_timeout' => 15,
 		],
-		/**
-		 * MediaWiki checks if the certificate presented by MariaDB is signed
-		 * by the certificate authority listed in 'sslCAFile'. In emergencies
-		 * this could be set to /etc/ssl/certs/ca-certificates.crt (all trusted
-		 * CAs), but setting this to one CA reduces attack vector and CAs
-		 * to dig through when checking the certificate provided by MariaDB.
-		 *
-		 * TEMPORARY: use ca-certificates.crt, as this doesn't seem to like LetsEncrypt.crt
-		 */
-		'sslCAFile' => '/etc/ssl/certs/ca-certificates.crt',
 	],
 	'hostsByName' => [
 		'db1' => $wmgDBHostname,

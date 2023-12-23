96768d80a99966f14491110816974a857e776e8e
CosmicAlpha <cosmic.alpha24@gmail.com>
2023-12-23 16:36:25 -0700

    Update mem server

--- GlobalCache.php
+++ GlobalCache.php
@@ -20,7 +20,7 @@ $wgObjectCaches['memcached'] = [
 ];
 
 // Used by WikiTideMagic for clearing keys when a wiki is deleted or renamed
-$wgWikiTideMagicMemcachedServer = '10.0.2.4:11211';
+$wgWikiTideMagicMemcachedServer = '10.0.0.106:11211';
 
 $wgObjectCaches['mysql-multiwrite'] = [
 	'class' => MultiWriteBagOStuff::class,

e0095ece9de4ec097ea7e31ec540ff9ff6dabdcd
Agent Isai <43097272+AgentIsai@users.noreply.github.com>
2024-03-28 11:55:44 -0500

    Use Sitenotice.php

--- LocalSettings.php
+++ LocalSettings.php
@@ -4775,6 +4775,7 @@ if ( $wgRequestTimeLimit ) {
 require_once '/srv/mediawiki/config/Database.php';
 require_once '/srv/mediawiki/config/GlobalCache.php';
 require_once '/srv/mediawiki/config/GlobalLogging.php';
+require_once '/srv/mediawiki/config/Sitenotice.php';
 require_once '/srv/mediawiki/config/FileBackend.php';
 
 if ( $wi->missing ) {
